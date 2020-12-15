ee5811d604468b5809ae8fae64df11c195fb60e6
Francois Theroux <ftheroux2@gmail.com>
2020-12-15 17:54:32 -0500

    Update gatsby-node.js

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,26 +1,30 @@
-const webpack = require("webpack")
+const webpack = require("webpack");
 
 function flattenMessages(nestedMessages, prefix = "") {
   return Object.keys(nestedMessages).reduce((messages, key) => {
-    let value = nestedMessages[key]
-    let prefixedKey = prefix ? `${prefix}.${key}` : key
+    let value = nestedMessages[key];
+    let prefixedKey = prefix ? `${prefix}.${key}` : key;
 
     if (typeof value === "string") {
-      messages[prefixedKey] = value
+      messages[prefixedKey] = value;
     } else {
-      Object.assign(messages, flattenMessages(value, prefixedKey))
+      Object.assign(messages, flattenMessages(value, prefixedKey));
     }
 
-    return messages
-  }, {})
+    return messages;
+  }, {});
 }
 
 exports.onCreateWebpackConfig = ({ actions, plugins }, pluginOptions) => {
-  const { redirectComponent = null, languages, defaultLanguage } = pluginOptions
+  const {
+    redirectComponent = null,
+    languages,
+    defaultLanguage,
+  } = pluginOptions;
   if (!languages.includes(defaultLanguage)) {
-    languages.push(defaultLanguage)
+    languages.push(defaultLanguage);
   }
-  const regex = new RegExp(languages.map(l => l.split("-")[0]).join("|"))
+  const regex = new RegExp(languages.map((l) => l.split("-")[0]).join("|"));
   actions.setWebpackConfig({
     plugins: [
       plugins.define({
@@ -35,63 +39,70 @@ exports.onCreateWebpackConfig = ({ actions, plugins }, pluginOptions) => {
         regex
       ),
     ],
-  })
-}
+  });
+};
 
 exports.onCreatePage = async ({ page, actions }, pluginOptions) => {
   //Exit if the page has already been processed.
   if (typeof page.context.intl === "object") {
-    return
+    return;
   }
-  const { createPage, deletePage } = actions
+  const { createPage, deletePage } = actions;
   const {
     path = ".",
     languages = ["en"],
     defaultLanguage = "en",
     redirect = false,
-  } = pluginOptions
+  } = pluginOptions;
 
   const getMessages = (path, language) => {
     try {
       // TODO load yaml here
-      const messages = require(`${path}/${language}.json`)
+      const messages = require(`${path}/${language}.json`);
 
-      return flattenMessages(messages)
+      return flattenMessages(messages);
     } catch (error) {
       if (error.code === "MODULE_NOT_FOUND") {
         process.env.NODE_ENV !== "test" &&
           console.error(
             `[gatsby-plugin-intl] couldn't find file "${path}/${language}.json"`
-          )
+          );
       }
 
-      throw error
+      throw error;
     }
-  }
+  };
 
   // Return all languages slug for this page
-  const getSlugs = path => {
-    var currentPageList = page.path.split("/")
+  const getSlugs = (path) => {
+    var currentPageList = page.path.split("/");
 
-    var slugs = {}
-    var pathtmp = ""
-    languages.forEach(language => {
-      var messages = getMessages(path, language)
-      currentPageList.forEach(currentPage => {
+    var slugs = {};
+    var pathtmp = "";
+    languages.forEach((language) => {
+      var messages = getMessages(path, language);
+      currentPageList.forEach((currentPage) => {
+        if (currentPage === "") {
+          break;
+        }
         pathtmp += messages[`${currentPage}.slug`]
           ? `/${messages[currentPage + ".slug"]}`
-          : `/${currentPage}`
-      })
-      console.log(pathtmp)
-      slugs[language] = pathtmp
-    })
-    return slugs
-  }
+          : `/${currentPage}`;
+      });
+      console.log(pathtmp);
+      slugs[language] = pathtmp;
+    });
+    return slugs;
+  };
 
   const generatePage = (routed, language) => {
-    const messages = getMessages(path, language)
-    const slugs = page.context.generatedContent ? page.context.slugs : getSlugs(path);
-    var newPath = routed ? `/${language}${slugs[language]}` : `${slugs[language]}`
+    const messages = getMessages(path, language);
+    const slugs = page.context.generatedContent
+      ? page.context.slugs
+      : getSlugs(path);
+    var newPath = routed
+      ? `/${language}${slugs[language]}`
+      : `${slugs[language]}`;
     return {
       ...page,
       path: newPath,
@@ -109,23 +120,29 @@ exports.onCreatePage = async ({ page, actions }, pluginOptions) => {
           defaultLanguage,
         },
       },
-    }
+    };
+  };
+
+  const newPage = generatePage(false, defaultLanguage);
+  deletePage(page);
+  if (
+    !page.context.generatedContent ||
+    (page.context.generatedContent && page.context.slugs[defaultLanguage])
+  ) {
+    createPage(newPage);
   }
 
-  const newPage = generatePage(false, defaultLanguage)
-  deletePage(page)
-  if (!page.context.generatedContent || (page.context.generatedContent && page.context.slugs[defaultLanguage])) {
-    createPage(newPage)
-  }
-
-  languages.forEach(language => {
-    if (page.context.generatedContent && page.context.generatedLanguage !== language)
+  languages.forEach((language) => {
+    if (
+      page.context.generatedContent &&
+      page.context.generatedLanguage !== language
+    )
       return;
-    const localePage = generatePage(true, language)
-    const regexp = new RegExp("/404/?$")
+    const localePage = generatePage(true, language);
+    const regexp = new RegExp("/404/?$");
     if (regexp.test(localePage.path)) {
-      localePage.matchPath = `/${language}/*`
+      localePage.matchPath = `/${language}/*`;
     }
-    createPage(localePage)
-  })
-}
+    createPage(localePage);
+  });
+};
